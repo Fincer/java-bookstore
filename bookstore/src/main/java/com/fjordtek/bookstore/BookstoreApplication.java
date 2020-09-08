9065513bbeced442891ad2f8b8612fe41586a834
Pekka Helenius <fincer89@hotmail.com>
2020-09-09 02:06:16 +0300

    Add support for independent Tomcat deployment
    Signed-off-by: Pekka Helenius <fincer89@hotmail.com>

--- bookstore/src/main/java/com/fjordtek/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/fjordtek/bookstore/BookstoreApplication.java
@@ -2,9 +2,10 @@ package com.fjordtek.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
