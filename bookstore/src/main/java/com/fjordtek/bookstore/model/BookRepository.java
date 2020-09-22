fb254e1e3d8d3e699ffe5a8d98cdb324763b806b
Pekka Helenius <fincer89@hotmail.com>
2020-09-23 01:29:57 +0300

    Initial REST API search support for a book by its title
    Signed-off-by: Pekka Helenius <fincer89@hotmail.com>

--- bookstore/src/main/java/com/fjordtek/bookstore/model/BookRepository.java
+++ bookstore/src/main/java/com/fjordtek/bookstore/model/BookRepository.java
@@ -2,14 +2,20 @@
 
 package com.fjordtek.bookstore.model;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long> {
 
 	@Override
 	public Optional<Book> findById(Long id);
+
+	public List<Book> findByTitle(@Param("title") String title);
 
 	/* Assume a single book with a single ISBN, or multiple books with possibly duplicate ISBNs?
 	 * For meanwhile, we have a UNIQUE constraint for ISBN values. If this policy changes,
