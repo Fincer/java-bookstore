e430378fdfb078b7ffefc56abb4f6911e0268bff
Pekka Helenius <fincer89@hotmail.com>
2020-09-23 01:21:16 +0300

    Modify findById method
    Signed-off-by: Pekka Helenius <fincer89@hotmail.com>

--- bookstore/src/main/java/com/fjordtek/bookstore/model/BookRepository.java
+++ bookstore/src/main/java/com/fjordtek/bookstore/model/BookRepository.java
@@ -2,13 +2,14 @@
 
 package com.fjordtek.bookstore.model;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
 
-	public List<Book> findById(String title);
+	@Override
+	public Optional<Book> findById(Long id);
 
 	/* Assume a single book with a single ISBN, or multiple books with possibly duplicate ISBNs?
 	 * For meanwhile, we have a UNIQUE constraint for ISBN values. If this policy changes,
