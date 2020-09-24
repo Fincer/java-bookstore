1459fb754b766ec54db9d35822cf1856ef7775a3
Pekka Helenius <fincer89@hotmail.com>
2020-09-24 10:41:17 +0300

    Add default redirect
    Signed-off-by: Pekka Helenius <fincer89@hotmail.com>

--- bookstore/src/main/java/com/fjordtek/bookstore/web/BookRestController.java
+++ bookstore/src/main/java/com/fjordtek/bookstore/web/BookRestController.java
@@ -6,10 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fjordtek.bookstore.model.Book;
@@ -25,6 +27,9 @@ public class BookRestController {
 	@Autowired
 	private CategoryRepository   categoryRepository;
 */
+
+	// TODO Use single variable reference for all controllers
+	private static final String bookListPageView      = "booklist";
 
 	private HttpServerLogger     httpServerLogger     = new HttpServerLogger();
 
@@ -56,5 +61,21 @@ public class BookRestController {
 
 		return bookRepository.findById(bookId);
 	}
+
+	//////////////////////////////
+	// REDIRECTS
+
+	@RequestMapping(
+			value    = { "*" }
+			)
+	@ResponseStatus(HttpStatus.FOUND)
+    public void redirectToDefaultWebForm(
+    		HttpServletRequest requestData,
+			HttpServletResponse responseData
+			) {
+    	httpServerLogger.log(requestData, responseData);
+    	responseData.setHeader("Location", "/" + bookListPageView);
+    	responseData.setStatus(302);
+    }
 
 }
