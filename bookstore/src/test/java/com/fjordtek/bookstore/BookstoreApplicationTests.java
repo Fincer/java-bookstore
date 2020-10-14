29fd4a1d2ec1b2f37b01a3cc7cd8bc333f21630f
Pekka Helenius <fincer89@hotmail.com>
2020-10-14 13:22:20 +0300

    Add dummy BookstoreApplicationTests class
    Signed-off-by: Pekka Helenius <fincer89@hotmail.com>

--- bookstore/src/test/java/com/fjordtek/bookstore/BookstoreApplicationTests.java
+++ bookstore/src/test/java/com/fjordtek/bookstore/BookstoreApplicationTests.java
@@ -1,8 +1,20 @@
+// Pekka Helenius <fincer89@hotmail.com>, Fjordtek 2020
+
 package com.fjordtek.bookstore;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ *
+ * TODO: N/A
+ *
+ * @author Pekka Helenius
+ */
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class BookstoreApplicationTests {
 
